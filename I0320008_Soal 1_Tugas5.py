2a117970dfdb05fab5e1874d22d9f9fb003f7375
Alifiana Rahma <alifianars03@gmail.com>
2021-04-02 21:52:03 +0700

    a

---
+++ I0320008_Soal 1_Tugas5.py
@@ -0,0 +1,6 @@
+nama = input("Masukkan Nama :")
+jk = input("Jenis Kelamin (L/P) :")
+if jk == "L" :
+    print("Selamat datang, Tuan",nama,"!")
+elif jk == "P" :
+    print("Selamat datang, Nyonya",nama,"!")
